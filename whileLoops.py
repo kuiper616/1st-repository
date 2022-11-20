ab6126e4c1fe96ad0a2b5bba61167c3fc357603f
kuiper616 <109433742+kuiper616@users.noreply.github.com>
2022-11-19 20:28:08 -0500

    Add files via upload

---
+++ whileLoops.py
@@ -0,0 +1,7 @@
+numTimes = input("how many A's would you like? ")
+x = 0
+while x < int(numTimes):
+    print("s")
+    x = x + 1
+string = input("program ended press any key to continue- ")
+print(string)
